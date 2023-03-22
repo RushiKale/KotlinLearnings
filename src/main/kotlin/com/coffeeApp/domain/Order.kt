2ff116059eb335e50574da05e2a3633fd2e65c22
RushiKale <kalerushikesh2161@gmail.com>
2023-03-22 23:39:14 +0530

    Win Commit 1st

--- src/main/kotlin/com/coffeeApp/domain/Order.kt
+++ src/main/kotlin/com/coffeeApp/domain/Order.kt
@@ -2,9 +2,20 @@ package com.coffeeApp.domain
 
 data class Order(val customerName : String) {
     val orderID = counter++
-    val orderItems : Map<Coffee,Int>
+    val orderItems : Map<Coffee,Int> = mutableMapOf()
 
     companion object {
         var counter = 100
     }
+
+    fun addCoffee(coffee: Coffee){
+        if(orderItems.containsKey(coffee)){
+            orderItems[coffee] = orderItems.get(coffee) + 1
+        }
+        else{
+            orderItems.put(coffee,1)
+        }
+    }
+
+
 }
